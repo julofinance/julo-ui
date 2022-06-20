cc42dab84082241ff46f48f8409493645d94750b
Kevin Giovanni <kevinevraldo@gmail.com>
2022-06-20 14:58:39 +0700

    feat: create workflow to publish package

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,4 +1,5 @@
 declare module '@julofinance/color-token';
+declare module '@testing-library/react';
 
 declare module '*.svg' {
   const content: string | React.FC<React.SVGAttributes<SVGElement>>;
