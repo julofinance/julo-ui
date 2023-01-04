ae3b7f6928974089e86574dc536d85d6c11b2ad4
Arga Tahta <arga@julofinance.com>
2023-01-04 16:51:29 +0700

    fix: add export on component core

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,4 +3,5 @@ export { default as CircleShimmer } from './CircleShimmer';
 export { default as LineShimmer } from './LineShimmer';
 export { default as Radio } from './Radio';
 export { default as Typography } from './Typography';
+export { default as Input } from './Input';
 export * from './Accordion';
