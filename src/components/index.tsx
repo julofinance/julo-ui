7a90a9dfdeebc4a22fce0015287448937eada3ba
Arga Tahta <arga@julofinance.com>
2023-01-04 16:53:01 +0700

    fix: add export dialog on component root folder

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,4 +3,5 @@ export { default as CircleShimmer } from './CircleShimmer';
 export { default as LineShimmer } from './LineShimmer';
 export { default as Radio } from './Radio';
 export { default as Typography } from './Typography';
+export { default as Dialog } from './Dialog';
 export * from './Accordion';
