a8a92f8d5e505b27c657293e7094f29354913bed
fhaga <fergus.hagaswara@julofinance.com>
2023-01-10 11:13:24 +0700

    add custom header example

--- src/components/BottomSheet/BottomSheet.stories.tsx
+++ src/components/BottomSheet/BottomSheet.stories.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { useArgs } from "@storybook/client-api";
+import { PR50 } from '@julofinance/color-token'
 import BottomSheet from './BottomSheet';
 import BottomSheetHeader from './BottomSheetHeader';
 import BottomSheetContent from './BottomSheetContent';
 import Typography from '../Typography';
 import docs from './readme.md'
 import BottomSheetFooter from './BottomSheetFooter';
+import { css } from '@emotion/css';
 
 export default {
   title: 'Components/BottomSheet',
@@ -213,4 +215,77 @@ export const WithFooter: ComponentStory<typeof BottomSheet> = ({onClose, ...args
 };
 WithFooter.args = {
   isOpen: false
+}
+
+export const CustomHeader: ComponentStory<typeof BottomSheet> = ({onClose, ...args}) => {
+  const [, updateArgs] = useArgs()
+
+  return (
+    <>
+      <button onClick={() => updateArgs({isOpen: true})}>Open Bottom Sheet</button>
+      <BottomSheet {...args} onClose={() => updateArgs({isOpen: false})}>
+        <BottomSheetHeader>
+          <div 
+            className={css`
+              display: flex;
+              align-items: center;
+              padding: 16px;
+              justify-content: space-between;
+            `} 
+          >
+            <Typography fontSize="16px" fontWeight={700}>
+              Bottom Sheet Title
+            </Typography>
+            <span 
+              className={css`
+                color: ${PR50};
+                cursor: pointer;
+              `} 
+              onClick={() => updateArgs({isOpen: false})}
+            >
+              &#x2715;
+            </span>
+          </div>
+        </BottomSheetHeader>
+        <BottomSheetContent>
+          <Typography fontSize="14px">
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
+            non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
+            non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
+            non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
+            non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
+            non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          </Typography>
+        </BottomSheetContent>
+        <BottomSheetFooter>
+          <button>
+            Bottom sheet footer
+          </button>
+        </BottomSheetFooter>
+      </BottomSheet>
+    </>
+  );
+};
+CustomHeader.args = {
+  isOpen: false,
+  showDragBar: false
 }
