bdf226e5d2b566853c22a8f9779da783e64c04ba
Fransiscus Hermanto <fransiscus.hermanto@julofinance.com>
2023-08-16 11:35:51 +0700

    feat(@julo-ui/provider): add shadows for theme foundations

--- packages/components/provider/src/theme-provider/foundations/index.ts
+++ packages/components/provider/src/theme-provider/foundations/index.ts
@@ -1,7 +1,8 @@
 import colors from './colors';
 import fontSizes from './font-sizes';
 import lineHeights from './line-heights';
+import shadows from './shadows';
 
-const DEFAULT_THEME = { colors, fontSizes, lineHeights };
+const DEFAULT_THEME = { colors, fontSizes, lineHeights, shadows };
 
 export { DEFAULT_THEME, colors, fontSizes, lineHeights };
--- packages/components/provider/src/theme-provider/styles/global-variable.ts
+++ packages/components/provider/src/theme-provider/styles/global-variable.ts
@@ -3,10 +3,11 @@ import { css } from '@emotion/react';
 import { Theme } from '../types';
 import { generateColorsCssVar, generateCommonCssVar } from '../utils';
 
-export default ({ colors, fontSizes, lineHeights }: Theme) => css`
+export default ({ colors, fontSizes, lineHeights, shadows }: Theme) => css`
   :root {
     ${generateColorsCssVar(colors)}
     ${generateCommonCssVar('fontSizes', fontSizes)}
     ${generateCommonCssVar('lineHeights', lineHeights)}
+    ${generateCommonCssVar('shadows', shadows)}
   }
 `;
