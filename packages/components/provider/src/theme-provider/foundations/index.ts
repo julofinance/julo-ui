29b5f85eb4939f17f007eb6fe102a89246a5e793
Fransiscus Hermanto <fransiscus.hermanto@julofinance.com>
2023-08-20 20:27:29 +0700

    feat(@julo-ui/provider): add radius as theme foundation

--- packages/components/provider/src/theme-provider/foundations/index.ts
+++ packages/components/provider/src/theme-provider/foundations/index.ts
@@ -2,7 +2,8 @@ import colors from './colors';
 import fontSizes from './font-sizes';
 import lineHeights from './line-heights';
 import shadows from './shadows';
+import radius from './radius';
 
-const DEFAULT_THEME = { colors, fontSizes, lineHeights, shadows };
+const DEFAULT_THEME = { colors, fontSizes, lineHeights, shadows, radius };
 
 export { DEFAULT_THEME, colors, fontSizes, lineHeights };
--- packages/components/provider/src/theme-provider/styles/global-variable.ts
+++ packages/components/provider/src/theme-provider/styles/global-variable.ts
@@ -3,11 +3,18 @@ import { css } from '@emotion/react';
 import { Theme } from '../types';
 import { generateColorsCssVar, generateCommonCssVar } from '../utils';
 
-export default ({ colors, fontSizes, lineHeights, shadows }: Theme) => css`
+export default ({
+  colors,
+  fontSizes,
+  lineHeights,
+  shadows,
+  radius,
+}: Theme) => css`
   :root {
     ${generateColorsCssVar(colors)}
     ${generateCommonCssVar('fontSizes', fontSizes)}
     ${generateCommonCssVar('lineHeights', lineHeights)}
     ${generateCommonCssVar('shadows', shadows)}
+    ${generateCommonCssVar('corner', radius)}
   }
 `;
