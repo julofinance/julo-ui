088815de969e25446d92b4963007126fb9de5133
Albertus IP <albertus.istora@ti.ukdw.ac.id>
2023-09-18 16:00:22 +0700

    style(@julo-ui/card): handle style for variant and padding for card children

--- packages/components/card/src/styles.ts
+++ packages/components/card/src/styles.ts
@@ -4,9 +4,31 @@ import { SystemStyleObject } from '@julo-ui/system';
 
 import { CardVariant } from './types';
 
-export const cardCx = css``;
+export const cardCx = css`
+  border-radius: 0.5rem;
+
+  width: 100%;
+
+  > :nth-child(odd) {
+    padding-top: 12px;
+    padding-bottom: 12px;
+  }
+  > :nth-last-child(1):nth-child(even) {
+    padding-top: 0;
+    padding-bottom: 12px;
+  }
+`;
 
 export const cardVariant: Record<CardVariant, SystemStyleObject> = {
-  border: {},
-  filled: {},
+  default: {
+    backgroundColor: 'var(--colors-neutrals-10)',
+    boxShadow: '0px 1px 16px 0px  rgba(0, 0, 0, 0.12)',
+  },
+  border: {
+    background: 'var(--colors-neutrals-10)',
+    border: '1px solid var(--colors-neutrals-30)',
+  },
+  filled: {
+    background: 'var(--colors-neutrals-20)',
+  },
 };
