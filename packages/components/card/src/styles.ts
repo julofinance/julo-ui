1d38a94c4ae686b39212e3d8899061b8fcfe1aa2
Albertus IP <albertus.istora@ti.ukdw.ac.id>
2023-09-27 17:47:02 +0700

    fix(@julo-ui/card): change nth-last-child to nth-last-of-type and remove whitespace

--- packages/components/card/src/styles.ts
+++ packages/components/card/src/styles.ts
@@ -13,7 +13,7 @@ export const cardCx = css`
     padding-top: 12px;
     padding-bottom: 12px;
   }
-  > :nth-last-child(1):nth-of-type(even) {
+  > :nth-last-of-type(1):nth-of-type(even) {
     padding-top: 0;
     padding-bottom: 12px;
   }
@@ -22,7 +22,7 @@ export const cardCx = css`
 export const cardVariant: Record<CardVariant, SystemStyleObject> = {
   default: {
     backgroundColor: 'var(--colors-neutrals-10)',
-    boxShadow: '0px 1px 16px 0px  rgba(0, 0, 0, 0.12)',
+    boxShadow: '0px 1px 16px 0px rgba(0, 0, 0, 0.12)',
   },
   border: {
     background: 'var(--colors-neutrals-10)',
