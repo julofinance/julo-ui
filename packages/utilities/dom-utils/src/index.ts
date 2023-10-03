caa2c9f27a250ba4c6c5d566081afead01d49fef
Fransiscus Hermanto <fransiscus.hermanto@julofinance.com>
2023-10-03 16:28:21 +0700

    feat(@julo-ui/dom-utils): add isRef util

--- packages/utilities/dom-utils/src/index.ts
+++ packages/utilities/dom-utils/src/index.ts
@@ -1,4 +1,5 @@
 export { default as ariaAttr } from './aria-attr';
 export { default as dataAttr } from './data-attr';
-export { default as mergeRefs } from './merge-refs';
 export { default as isInputEvent } from './is-input-event';
+export { default as isRef } from './is-ref';
+export { default as mergeRefs } from './merge-refs';
