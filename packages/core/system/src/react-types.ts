dfb2a328b062290fa002b60e99ee0aedecfa1995
Fransiscus Hermanto <fransiscus.hermanto@julofinance.com>
2023-10-06 15:13:54 +0700

    docs(@julo-ui/system): enhance typing for Merged, PropGetter and RequiredPropGetter

--- packages/core/system/src/react-types.ts
+++ packages/core/system/src/react-types.ts
@@ -25,17 +25,14 @@ export type DOMAttributes<T = DOMElement> = React.AriaAttributes &
     style?: React.CSSProperties;
   };
 
-type Merge<M, N> = N extends Record<string, unknown> ? M : Omit<M, keyof N> & N;
+type Merge<M, N> = N extends Record<string, unknown> ? Omit<M, keyof N> & N : M;
 
-export type PropGetter<P = Record<string, unknown>, R = DOMAttributes> = (
+export type PropGetter<P = DOMAttributes, R = DOMAttributes> = (
   props?: Merge<DOMAttributes, P>,
   ref?: React.Ref<any>,
 ) => R & React.RefAttributes<any>;
 
-export type RequiredPropGetter<
-  P = Record<string, unknown>,
-  R = DOMAttributes,
-> = (
+export type RequiredPropGetter<P = DOMAttributes, R = DOMAttributes> = (
   props: Merge<DOMAttributes, P>,
   ref?: React.Ref<any>,
 ) => R & React.RefAttributes<any>;
