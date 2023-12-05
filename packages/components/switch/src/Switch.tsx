2917a08c36a170726646862ed16fefa9767f6717
Fransiscus Hermanto <fransiscus.huang47@gmail.com>
2023-12-05 13:40:53 +0700

    feat(@julo-ui/switch): v0.0.1

--- packages/components/switch/src/Switch.tsx
+++ packages/components/switch/src/Switch.tsx
@@ -1,13 +1,40 @@
-import { forwardRef, julo } from '@julo-ui/system';
+import { cx, forwardRef, julo } from '@julo-ui/system';
+import { useCheckbox } from '@julo-ui/checkbox';
 
 import { switchCx } from './styles';
 import type { SwitchProps } from './types';
 
 const Switch = forwardRef<SwitchProps, 'div'>((props, ref) => {
+  const { className, children, inputProps, inputRef, ...resProps } = props;
+
+  const {
+    getRootProps,
+    getIndicatorProps,
+    getCheckboxProps,
+    getLabelProps,
+    getInputProps,
+  } = useCheckbox(resProps);
+
   return (
-    <julo.div ref={ref} __css={switchCx}>
-      Hello World
-    </julo.div>
+    <julo.label
+      ref={ref}
+      className={cx('julo-switch', className)}
+      {...getRootProps()}
+      __css={switchCx}
+    >
+      <julo.input
+        className='julo-switch__input'
+        {...getInputProps(inputProps, inputRef)}
+      />
+      <julo.span {...getCheckboxProps()} className='julo-switch__track'>
+        <julo.span {...getIndicatorProps()} className='julo-switch__thumb' />
+      </julo.span>
+      {children && (
+        <julo.span className='julo-switch__label' {...getLabelProps()}>
+          {children}
+        </julo.span>
+      )}
+    </julo.label>
   );
 });
 
--- packages/components/switch/src/types.ts
+++ packages/components/switch/src/types.ts
@@ -1,1 +1,12 @@
-export interface SwitchProps {}
+import type { DOMAttributes, HTMLJuloProps } from '@julo-ui/system';
+import type { UseCheckboxProps } from '@julo-ui/checkbox';
+
+export interface SwitchProps
+  extends Omit<UseCheckboxProps, 'isIndeterminate'>,
+    Omit<HTMLJuloProps<'label'>, keyof UseCheckboxProps> {
+  /**
+   * Additional props to be forwarded to the `input` element
+   */
+  inputProps?: DOMAttributes<HTMLInputElement>;
+  inputRef?: React.RefObject<HTMLInputElement>;
+}
