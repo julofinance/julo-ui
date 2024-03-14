92992c0f97f1b6a758cc132b22ed7a48f13dacc5
Fransiscus Hermanto <fransiscus.huang47@gmail.com>
2024-03-14 16:51:59 +0700

    fix(@julo-ui/checkbox): export RadioGroupContext items

--- packages/components/checkbox/src/index.ts
+++ packages/components/checkbox/src/index.ts
@@ -3,10 +3,6 @@ export { default as Checkbox } from './Checkbox';
 export type { CheckboxProps, CheckboxState, UseCheckboxProps } from './types';
 export { CheckboxIcon } from './components/checkbox-icon';
 export type { CheckboxIconProps } from './components/checkbox-icon';
-export { CheckboxGroup, useCheckboxGroup } from './components/checkbox-group';
-export type {
-  CheckboxGroupProps,
-  UseCheckboxGroupProps,
-} from './components/checkbox-group';
+export * from './components/checkbox-group';
 export { useCheckbox } from './use-checkbox';
 export type { UseCheckboxReturn } from './use-checkbox';
